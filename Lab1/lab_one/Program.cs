297a3dc16bf0fd4160f3f0078a3ede5ca90f42d6
Darkoment <Darkoment@yandex.ru>
2022-04-22 18:55:22 +0300

    Add: Lab1

---
+++ Lab1/lab_one/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace lab_one
+{
+    class Program {
+
+        static void Main(string[] args) {
+            int a = 0, b = 1; // переменные для чисел фибоначчи
+            int c; //переменная для хранения суммы чисел фибоначчи
+            int n; // переменная для ввода последовательности
+            Console.WriteLine("введите до какой последовательности нужно вывести числа:");
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(n);
+            for (int i=0; i < n; i++) {
+                c = a + b;
+                Console.WriteLine($"{a} {b} сумма чисел: {c}");
+                a = b;
+                b = c;
+            }
+            
+            Console.WriteLine("Конец программы");
+            
+        }
+    }
+}
