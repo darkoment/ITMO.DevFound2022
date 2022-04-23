6eecac48ee7bfa95cb941146fcac6156cdf132b8
Darkoment <Darkoment@yandex.ru>
2022-04-23 16:49:35 +0300

    Added Lab2

---
+++ Lab2/lab_two/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace lab_two 
+{
+    class Program {
+        static void Main(string[] args) {
+            // int First = 1;
+            int Min;
+            // int Current;
+            int[] Mass = new int[] {4, 7, 8, 3, 1, 4, 6, 7, 33, 12, 45, 43, 12, 43, 1};
+            int Temp;
+
+            for (int i = 0; i < Mass.Length; i++) {
+                Console.Write($"{Mass[i]} ");
+            }
+                Console.WriteLine();
+
+            for (int First = 0; First < Mass.Length - 1; First++) {
+                for (Min = First + 1; Min < Mass.Length; Min++) {
+                    if (Mass[First] > Mass[Min]) {
+                        Temp = Mass[First];
+                        Mass[First] = Mass[Min];
+                        Mass[Min] = Temp;
+                    }
+                }
+            }
+
+            Console.WriteLine("Готовый Массив: ");
+            for (int i = 0; i < Mass.Length; i++) {
+                Console.Write($"{Mass[i]} ");
+            }
+
+        }
+    }
+}
